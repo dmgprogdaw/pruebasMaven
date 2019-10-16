204523b9aa01d27c4028113d391d6d28e4899ef1
David <zukoclase96@gmail.com>
2019-10-16 11:45:34 +0200

    Creado el repositorio pruebasMaven

---
+++ src/main/java/pruebasMaven/modelo/CreateAlumno.java
@@ -0,0 +1,24 @@
+package pruebasMaven.modelo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import pruebasMaven.negocio.AlumnoBean;
+
+public class CreateAlumno {
+
+	public void create(AlumnoBean alumno) {
+
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.david.pruebasMaven.H2");
+		EntityManager entityManager = emf.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(alumno);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+				
+	}
+
+}
